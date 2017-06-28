b8a16b3081388ae4a100fc167ad641402831e90f
Brandon Olivares <b.olivares@inpsyde.com>
2017-06-28 11:09:16 +0000

    Add independence day notice

--- assets/templates/admin-notices/independence-day.php
+++ assets/templates/admin-notices/independence-day.php
@@ -1,8 +1,7 @@
 <p>
 	<?php
 	echo esc_html__(
-		'Get your 30% Independence Day discount for BackWPup Pro! ' .
-		'Only available until July 5th, 2017.',
+		'Get your 30% Independence Day discount for BackWPup Pro! Only available until July 5th, 2017.',
 		'backwpup'
 	);
 	?>
