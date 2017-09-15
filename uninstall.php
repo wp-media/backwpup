528e241516648e4f1a1d56770cfac367e9b54b55
duongcuong96 <duongcuong96@gmail.com>
2017-09-15 09:07:45 +0700

    Remove BackWPUp user roles after plugin deactivation

--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,32 @@
 <?php
 //if uninstall not called from WordPress exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die();
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die();
 }
 
 global $wpdb;
 /* @var wpdb $wpdb */
 
 //only uninstall if no BackWPup Version active
-if ( ! class_exists( 'BackWPup' ) ) {
+if (!class_exists('BackWPup')) {
 
-	//delete plugin options
-	if ( is_multisite() )
-		$wpdb->query( "DELETE FROM " . $wpdb->sitemeta . " WHERE meta_key LIKE '%backwpup_%' " );
-	else
-		$wpdb->query( "DELETE FROM " . $wpdb->options . " WHERE option_name LIKE '%backwpup_%' " );
+    //delete plugin options
+    if (is_multisite()) {
+        $wpdb->query("DELETE FROM " . $wpdb->sitemeta . " WHERE meta_key LIKE '%backwpup_%' ");
+    } else {
+        $wpdb->query("DELETE FROM " . $wpdb->options . " WHERE option_name LIKE '%backwpup_%' ");
+    }
+    
+    //delete Backwpup user roles 
+    $backWPUpRoles = array(
+        "backwpup_admin",
+        "backwpup_check",
+        "backwpup_helper"
+    );
+    
+    foreach( $backWPUpRoles as $backWPUpRole ){
+       if( get_role($backWPUpRole) ){
+           remove_role($backWPUpRole);
+       } 
+    }
 }
