7384a3b712dafd4fb8ed152118c0778c80c5325d
Brandon Olivares <b.olivares@inpsyde.com>
2017-10-26 10:57:19 -0400

    Remove user rolls on uninstall.

--- uninstall.php
+++ uninstall.php
@@ -15,4 +15,18 @@ if ( ! class_exists( 'BackWPup' ) ) {
 		$wpdb->query( "DELETE FROM " . $wpdb->sitemeta . " WHERE meta_key LIKE '%backwpup_%' " );
 	else
 		$wpdb->query( "DELETE FROM " . $wpdb->options . " WHERE option_name LIKE '%backwpup_%' " );
+
+	//delete Backwpup user roles 
+	$backWPUpRoles = array(
+		"backwpup_admin",
+		"backwpup_check",
+		"backwpup_helper"
+	);
+	
+	foreach ( $backWPUpRoles as $backWPUpRole ) {
+		if ( get_role( $backWPUpRole ) ) {
+			remove_role( $backWPUpRole );
+		}
+	}
+
 }
