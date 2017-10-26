038ad182ea9110e13095eed81b34ca5d768c42b0
Brandon Olivares <b.olivares@inpsyde.com>
2017-10-26 14:21:59 +0000

    Remove old notice information.

--- assets/templates/admin-notices/beta-tester.php
+++
@@ -1,8 +0,0 @@
-<p>
-	<?php
-	echo esc_html__(
-		'To ensure that our releases are as bug-free as possible, we need you as a beta tester!',
-		'backwpup'
-	);
-	?>
-</p>
--- assets/templates/admin-notices/independence-day.php
+++
@@ -1,8 +0,0 @@
-<p>
-	<?php
-	echo esc_html__(
-		'Get your 30% Independence Day discount for BackWPup Pro! Only available until July 5th, 2017.',
-		'backwpup'
-	);
-	?>
-</p>
